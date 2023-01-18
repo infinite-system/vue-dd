6c121e510cd4519404503b5cc9d94ab0b3ff7f70
Evgeny Kalashnikov <ekalashnikov@gmail.com>
2023-01-17 21:45:51 -0500

    added script to deploy to github pages

---
+++ docs/scripts/gh-pages-deploy.js
@@ -0,0 +1,32 @@
+import { execa } from 'execa'
+// import fs from "fs"
+
+(async () => {
+  try {
+    var datetime = new Date();
+
+    await execa("git", ["stash", "push", "-m", "before deploy on " + datetime]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+
+    console.log("Building...")
+
+    await execa("yarn", ["run", "build"])
+    // Understand if it's dist or build folder
+    const folderName = "docs/.vitepress/dist"
+    await execa("git", ["--work-tree", folderName, "add", "--all"])
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"])
+
+    console.log("Pushing to gh-pages...")
+
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"])
+    await execa("rm", ["-r", folderName])
+    await execa("git", ["checkout", "-f", "main"])
+    await execa("git", ["branch", "-D", "gh-pages"])
+
+    console.log("Successfully deployed")
+  } catch (e) {
+
+    console.log(e.message)
+    process.exit(1)
+  }
+})();
