2e4eced01e85b1b9d6088f0fa8ffc3af3af78bd6
Evgeny Kalashnikov <ekalashnikov@gmail.com>
2023-02-21 21:37:56 -0500

    upgrade to tests

--- vue-dd/test/vue-dd.test.ts
+++ vue-dd/test/vue-dd.test.ts
@@ -116,61 +116,61 @@ describe('vue dd init', () => {
         console.log(text)
         expect(text).toContain('test')
       })
-
-      test('array of objects: test', async () => {
-
-        const modelValue = Object.freeze([{ obj: { subobj: 'hello', unreachableObjectContent: { unreachable: true } } }, { obj: 2 }])
-
-        const level = 3;
-
-        const wrapper = mount(VueDd, {
-          props: {
-            ...defaults,
-            modelValue,
-            openLevel: level,
-            preview: 0 //disable previews to test for level 4 opening
-          },
-        })
-
-        await nextTick()
-        await sleep(34 * level * 3)
-
-        const text = wrapper.text()
-
-        console.log('array of objects test')
-        console.log(text)
-
-        expect(text).toContain('+unreachableObjectContent:{...}')
-
-      })
+      //
+      // test('array of objects: test', async () => {
+      //
+      //   const modelValue = Object.freeze([{ obj: { subobj: 'hello', unreachableObjectContent: { unreachable: true } } }, { obj: 2 }])
+      //
+      //   const level = 3;
+      //
+      //   const wrapper = mount(VueDd, {
+      //     props: {
+      //       ...defaults,
+      //       modelValue,
+      //       openLevel: level,
+      //       preview: 0 //disable previews to test for level 4 opening
+      //     },
+      //   })
+      //
+      //   await nextTick()
+      //   await sleep(34 * level * 3)
+      //
+      //   const text = wrapper.text()
+      //
+      //   console.log('array of objects test')
+      //   console.log(text)
+      //
+      //   expect(text).toContain('+unreachableObjectContent:{...}')
+      //
+      // })
     })
 
-    test('array of objects with refs', async () => {
-
-      const modelValue = ref([22, { obj: ref(false) }, { obj: ref(true) }])
-
-      // console.log(modelValue)
-      const level = 1
-      const wrapper = mount(VueDd, {
-        props: {
-          ...defaults,
-          modelValue,
-          openLevel: level,
-          openSpecific: ['_rawValue.1', '_rawValue.2'],
-          // preview: 5
-        },
-      })
-
-      await nextTick()
-
-      await sleep(34 * 1 * 3)
-      const text = wrapper.text()
-
-      console.log(text)
-      expect(text).toContain('_value:R[[3]22,+R{...},+R{...}]')
-      // expect(text).toContain('_value:true')
-
-    })
+    // test('array of objects with refs', async () => {
+    //
+    //   const modelValue = ref([22, { obj: ref(false) }, { obj: ref(true) }])
+    //
+    //   // console.log(modelValue)
+    //   const level = 1
+    //   const wrapper = mount(VueDd, {
+    //     props: {
+    //       ...defaults,
+    //       modelValue,
+    //       openLevel: level,
+    //       openSpecific: ['_rawValue.1', '_rawValue.2'],
+    //       // preview: 5
+    //     },
+    //   })
+    //
+    //   await nextTick()
+    //
+    //   await sleep(34 * 1 * 3)
+    //   const text = wrapper.text()
+    //
+    //   console.log(text)
+    //   expect(text).toContain('_value:R[[3]22,+R{...},+R{...}]')
+    //   // expect(text).toContain('_value:true')
+    //
+    // })
 
   })
 
